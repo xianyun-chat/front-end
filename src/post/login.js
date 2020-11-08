4d6027ab87f72f65ae6769ebbe7896113b3a13f5
gn1234 <37886159+gn1234@users.noreply.github.com>
2020-11-08 12:16:33 +0800

    Update login.js

--- src/post/login.js
+++ src/post/login.js
@@ -1,18 +1,22 @@
 import $ from 'jquery'
-export function login(userID,password,claaback){
+export function login(userID,password,callback){
+	
 	    $.ajax({
 	            url: "http://49.235.190.178:10010/api/login",
 	            type: "post",
 				data: {
-				  userID:userID,
-	              userName:password
+			       userID:userID,
+	                       userName:password
 	             },
 	            async:false,
 	            success: function (data) {
+                            if(data.code=200)
 				console.log(data);
-				},
+                                callback(true);
+			        },
 	            fail: function(error){
 			      console.log(error);
+			      
 	            }
 	        });
 }
